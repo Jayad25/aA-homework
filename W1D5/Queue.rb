2eb4063cd237ec7a0a728bd086c94cb17d0ab82b
Jaya <jayal2905@gmail.com>
2018-10-25 20:55:04 -0700

    Add instance methods

--- W1D5/Queue.rb
+++ W1D5/Queue.rb
@@ -1,2 +1,27 @@
 class Queue
+   attr_reader :queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue.last
+  end
 end
+
+queue = Queue.new
+
+queue.enqueue(3)
+queue.enqueue(4)
+queue.enqueue(7)
+queue.enqueue(5)
+p queue.dequeue
+p queue.peek
