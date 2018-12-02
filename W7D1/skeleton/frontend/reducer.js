fc6da5c9d054fb8b72cd203507fb008e5d6eb75b
Jaya <jayal2905@gmail.com>
2018-12-02 09:36:37 -0800

    initial react reducx

---
+++ W7D1/skeleton/frontend/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  city: "Please Select",
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  // return state; // remove this and fill out the body of the reducer function
+  switch (action.type) {
+    case "SWITCH_LOCATION":
+        return {
+          city: action.city,
+          jobs:action.jobs
+        };
+      // break;
+    default:
+    return state;
+
+  }
+};
+
+export default reducer;
